20aac079fa0083305ee8ef5fa8b398d41d162436
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-06-19 22:52:45 +0530

    REST Web Services 26 - Sending Status Codes and Location Headers

--- src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/messenger/resources/MessageResource.java
@@ -1,5 +1,6 @@
 package org.koushik.javabrains.messenger.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -11,7 +12,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import org.koushik.javabrains.messenger.model.Message;
 import org.koushik.javabrains.messenger.service.MessageService;
@@ -37,8 +41,15 @@ public class MessageResource {
 	}
 	
 	@POST
-	public Message addMessage(Message message) {
-		return messageService.addMessage(message);
+	public Response addMessage(Message message, @Context UriInfo uriInfo ) {
+		
+		Message newMessage = messageService.addMessage(message);
+		String newid = String.valueOf(newMessage.getId());
+		URI uri = uriInfo.getAbsolutePathBuilder().path(newid).build();
+		return Response.created(uri)
+				.entity(newMessage)
+				.build();
+//		return messageService.addMessage(message);
 	}
 	
 	@PUT
